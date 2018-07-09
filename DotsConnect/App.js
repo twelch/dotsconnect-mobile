dc9f7417bfea5e59f1f59a2c1ccdc0d7d74ba929
Tim Welch <tim.j.welch@gmail.com>
2018-07-09 00:42:35 -0700

    Add ReactNavigation screens and bottom bar with icons

--- DotsConnect/App.js
+++ DotsConnect/App.js
@@ -12,6 +12,8 @@ import {
   View,
   WebView
 } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Mapbox from '@mapbox/react-native-mapbox-gl'
 
 Mapbox.setAccessToken('pk.eyJ1IjoidHdlbGNoIiwiYSI6ImNqYzVxYTJ6NTF2NWUyeHBmNjcwdWwxY28ifQ.ug4rD1lc-yvGduyTkO18UA');
@@ -28,17 +30,45 @@ const webViewStyle = Platform.select({
   android: {}
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Text>Home Screen</Text>
+      </View>
+    );
+  }
+}
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home</Text>
+      </View>
+    );
+  }
+}
+
+class MapScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
         <Mapbox.MapView
           styleURL={Mapbox.StyleURL.Street}
           zoomLevel={15}
           centerCoordinate={[11.256, 43.770]}
           style={styles.container}>
         </Mapbox.MapView>
+      </View>
+    );
+  }
+}
+
+class BlogScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
         <WebView
           source={{uri: 'https://dotsconnect.us'}}
           style={webViewStyle}
@@ -52,4 +82,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   }
+});
+
+export default createBottomTabNavigator({
+  Home: HomeScreen,
+  Map: MapScreen,
+  Blog: BlogScreen
+}, {
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      if (routeName === 'Home') {
+        iconName = 'ios-home';
+      } else if (routeName === 'Map') {
+        iconName = 'ios-navigate';
+      } else if (routeName === 'Blog') {
+        iconName = 'ios-paper'
+      }
+
+      // You can return any component that you like here! We usually use an
+      // icon component from react-native-vector-icons
+      return <Ionicons name={iconName} size={25} color={tintColor} />;
+    },
+  }),
+  tabBarOptions: {
+    activeTintColor: '#338182',
+    inactiveTintColor: 'gray',
+  },
 });
