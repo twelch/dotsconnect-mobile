0a6aafb0d2e5f3b6da29f7c81f95c1d63beb847c
Tim Welch <tim.j.welch@gmail.com>
2018-07-09 23:00:58 -0700

    Add pulsing logo to home screen

--- DotsConnect/App.js
+++ DotsConnect/App.js
@@ -8,9 +8,10 @@ import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
-  Text,
   View,
-  WebView
+  WebView,
+  Animated,
+  Image
 } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -18,24 +19,41 @@ import Mapbox from '@mapbox/react-native-mapbox-gl'
 
 Mapbox.setAccessToken('pk.eyJ1IjoidHdlbGNoIiwiYSI6ImNqYzVxYTJ6NTF2NWUyeHBmNjcwdWwxY28ifQ.ug4rD1lc-yvGduyTkO18UA');
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class PulseImage extends React.Component {
+  state = {
+    animValue: new Animated.Value(20),  // Initial value
+  }
 
-const webViewStyle = Platform.select({
-  ios: {marginTop: 20},
-  android: {}
-});
+  componentDidMount() {
+    this.startAnimation()
+  }
 
-class App extends Component {
+  startAnimation() {
+    this.state.animValue.setValue(0);
+    Animated.timing(                  // Animate over time
+      this.state.animValue,            // The animated value to drive
+      {
+        toValue: 1,                   // Animate to opacity: 1 (opaque)
+        duration: 2500,              // Make it take a while
+      }
+    ).start(this.startAnimation.bind(this));                        // Starts the animation
+  }
+
   render() {
+    const marginValue = this.state.animValue.interpolate({
+      inputRange: [0, 0.5, 0.998, 0.999, 0.999, 1],
+      outputRange: [60, 54, 60, 60, 60, 60]
+    });
+
     return (
-      <View style={styles.container}>
-        <Text>Home Screen</Text>
-      </View>
+      <Animated.Image                 // Special animatable View
+        style={{
+          flex:1, height: undefined, width: undefined,
+          margin: marginValue,         // Bind opacity to animated value
+        }}
+        resizeMode="contain"
+        source={require('./assets/dc-logo-color-1024.png')}
+      />
     );
   }
 }
@@ -43,8 +61,8 @@ class App extends Component {
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home</Text>
+      <View style={{ flex: 1 }}>
+        <PulseImage />
       </View>
     );
   }
@@ -67,6 +85,11 @@ class MapScreen extends React.Component {
 
 class BlogScreen extends React.Component {
   render() {
+    const webViewStyle = Platform.select({
+      ios: {marginTop: 20},
+      android: {}
+    });
+
     return (
       <View style={{ flex: 1 }}>
         <WebView
